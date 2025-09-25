5d3fb0d9aee241184cc4a6ab89173ee6d2abd12a
Salscodess <123512670+salscodess@users.noreply.github.com>
2025-09-25 14:02:49 +0000

    Game Works

--- dice.py
+++ dice.py
@@ -13,12 +13,30 @@ while play == True:
     print("3: Quit")
     start = int(input(": "))
     if start == 1:
-        clear_terminal()
-        print("...")
-        
+        play_dice = True
+        while play_dice == True:
+            clear_terminal()
+            dice1 = random.randint(1,6)
+            dice2 = random.randint(1,6)
+            dice3 = random.randint(1,6)
+            dice4 = random.randint(1,6)
+            dice_result = dice1 + dice2
+            dice_result2 = dice3 + dice4
+            print(f"You rolled a {dice_result} and the computer rolled a {dice_result2}")
+            if dice_result > dice_result2:
+                print("You Won!")
+            elif dice_result < dice_result2:
+                print("You lost!")
+            elif dice_result == dice_result2:
+                print("It was a tie!")
+            time.sleep(1.5)
+            print("")
+            play_dice_input = str(input("Continue? (Y or N): ")).lower()
+            if play_dice_input == "n":
+                play_dice = False
+            clear_terminal()
     elif start == 2:
-        clear_terminal()
-        print("S")
+        print("--Settings--")
     elif start == 3:
         print("Bye")
         time.sleep(1.5)
